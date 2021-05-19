e372a34e8907d65a1f9be421cbff22b2dc8a0607
Sergey Stepin <sergey.stepin.dv@gmail.com>
2021-05-19 21:44:23 +1000

    доработан счетчик на go

--- counter.go
+++ counter.go
@@ -1,46 +1,88 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
+	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
+	"strconv"
 )
 
-func main() {
+func readCurrentDir(dirName string) int {
 
 	var result = 0
 
-	err := filepath.Walk(".",
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-
-			if !info.IsDir() {
-				var buffer bytes.Buffer
-				buffer.WriteString(path)
-
-				fmt.Println(buffer.String())
-
-				fContent, err := ioutil.ReadFile(path)
-				if err != nil {
-					panic(err)
-				}
-				asInt := int(fContent[0])
-
-				result = result + asInt
-
-			}
-
-			return nil
-		})
-
-	fmt.Println(result)
+	file, err := os.Open(dirName)
 
 	if err != nil {
-		log.Println(err)
+		log.Fatalf("failed opening directory: %s", err)
 	}
+
+	defer file.Close()
+
+	list, _ := file.Readdirnames(0) // 0 to read all files and folders
+
+	for _, name := range list {
+
+		var path = dirName + "/" + name
+
+		fi, err := os.Stat(path)
+
+		if err != nil {
+			fmt.Println(err)
+			return 0
+		}
+
+		switch mode := fi.Mode(); {
+
+		case mode.IsDir():
+			result += readCurrentDir(path)
+		case mode.IsRegular():
+
+			if name == "count" {
+
+				file, err := os.Open(path)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				defer file.Close()
+
+				scanner := bufio.NewScanner(file)
+				for scanner.Scan() {
+					var value = scanner.Text()
+
+					var strToInt, err = strconv.ParseInt(value, 10, 64)
+
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					result += int(strToInt)
+
+				}
+
+				if err := scanner.Err(); err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+	}
+	return result
+}
+
+func main() {
+
+	var path string
+
+	flag.StringVar(&path, "p", "/tmp/counter", "вспомогательное сообщение для flagname")
+	flag.Parse()
+
+	println(path)
+
+	var result = readCurrentDir(path)
+
+	println(result)
 }
